aee9888937c6de342d86492cbd59ca1fce2a7a4b
Adam Sawicki <sawicki.adam.j@gmail.com>
2025-11-04 22:24:11 -0500

    add some more aliases, a zellij entrypoint, and vscode extensions

--- home/adam.nix
+++ home/adam.nix
@@ -2,12 +2,10 @@
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 
-  ############################
-  # Packages (no HM module)  #
-  ############################
   home.packages = with pkgs; [
     # GUI apps
     brave
+    obsidian
     spotify
 
     # CLI tools (no HM module)
@@ -15,10 +13,15 @@
     eza
     jq
     ripgrep
+    zellij
 
     # Coding Assistents
     codex
+
+    # Allows nix vscode extension to work on autoformat
+    nixfmt-classic
   ];
+
 
   ############################
   # Program modules (HM-managed install + config)
@@ -38,22 +41,38 @@
       bindkey -M emacs '^R' fzf-history-widget
       bindkey '^[[1;5C' forward-word   # Ctrl →
       bindkey '^[[1;5D' backward-word  # Ctrl ←
+
+      # only run for interactive shells
+      if [[ $- == *i* ]]; then
+      # don't start a zellij inside a zellij
+        if [ -z "$ZELLIJ" ]; then
+          # attach to "main" or create it
+          zellij attach -c main
+          # stop this shell so we don't have a zsh running underneath
+          exit
+        fi
+    fi
     '';
     shellAliases = {
-      ga   = "git add";
-      gb   = "git branch";
-      gc   = "git commit -m";     # (ok, just be aware some folks use gc for 'git clean')
-      gd   = "git diff";
-      gdc  = "git diff --cached";
-      goo  = "git checkout";
-      gp   = "git push";
-      gs   = "git status";
-      gu   = "git reset HEAD~";
-      gcan = "git commit --amend --no-edit";
-      gau  = "git add -u .";
+      ga   =  "git add";
+      gb   =  "git branch";
+      gc   =  "git commit -m";     # (ok, just be aware some folks use gc for 'git clean')
+      gd   =  "git diff";
+      gdc  =  "git diff --cached";
+      goo  =  "git checkout";
+      gp   =  "git push";
+      gs   =  "git status";
+      gu   =  "git reset HEAD~";
+      gcan =  "git commit --amend --no-edit";
+      gau  =  "git add -u .";
       ghack = "git add -u . && git commit --amend --no-edit && git push -f";
-      glf  = "git log --format=oneline";
+      glf  =  "git log --format=oneline";
+
+      cnix = "sudo code /etc/nixos --no-sandbox --user-data-dir ~/.sudo-vscode";
+      nbs  = "sudo nixos-rebuild switch";
+      gnix = ''git -C /etc/nixos add -A && git -C /etc/nixos commit -m "$@"'';
     };
+
   };
 
   programs.fzf = {
@@ -70,8 +89,10 @@
       enableUpdateCheck = false;
       extensions = with pkgs.vscode-extensions; [
         esbenp.prettier-vscode
-        ms-python.python
+        golang.go
         jnoortheen.nix-ide
+        mkhl.direnv
+        ms-python.python
       ];
       userSettings = {
         "editor.formatOnSave" = true;
